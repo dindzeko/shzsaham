7d8569886397487a0537815ed3b776d574d6197e
dindzeko <127524116+dindzeko@users.noreply.github.com>
2025-08-11 15:18:27 +0700

    Update pattern_detector.py

--- modules/pattern_detector.py
+++ modules/pattern_detector.py
@@ -1,1 +1,29 @@
+# modules/pattern_detector.py
 
+def detect_pattern(data):
+    """
+    Mendeteksi pola "Pisau Jatuh" atau pola bearish continuation.
+    Pola: 1 bullish candle, diikuti 3 bearish candle, dalam uptrend.
+    """
+    if len(data) < 4:
+        return False
+        
+    recent = data.tail(4)
+    c1, c2, c3, c4 = recent.iloc[0], recent.iloc[1], recent.iloc[2], recent.iloc[3]
+
+    # Kriteria pola
+    is_c1_bullish = c1['Close'] > c1['Open'] and (c1['Close'] - c1['Open']) > 0.015 * c1['Open']
+    is_c2_bearish = c2['Close'] < c2['Open'] and c2['Close'] < c1['Close']
+    is_c3_bearish = c3['Close'] < c3['Open']
+    is_c4_bearish = c4['Close'] < c4['Open']
+    is_uptrend = data['Close'].iloc[-20:].mean() > data['Close'].iloc[-50:-20].mean() if len(data) >= 50 else False
+    is_close_sequence = c2['Close'] > c3['Close'] > c4['Close']
+
+    return all([
+        is_c1_bullish,
+        is_c2_bearish,
+        is_c3_bearish,
+        is_c4_bearish,
+        is_uptrend,
+        is_close_sequence
+    ])
